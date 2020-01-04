a95a5c2d8e9ad1afaea6ac69011b1b678bfa92bf
YifanTian <yifant@uci.edu>
2020-01-03 18:09:47 -0800

    add2

--- index.js
+++ index.js
@@ -74,7 +74,6 @@ app.get('/api/getList', (req,res) => {
 // Handles any requests that don't match the ones above
 app.get('*', (req,res) =>{
 	res.sendFile(path.join(__dirname+'/client/build/index.html'));
-	res.sendFile(path.join(__dirname+'/client/build/cucs.png'));
 });
 
 const port = process.env.PORT || 8081;
