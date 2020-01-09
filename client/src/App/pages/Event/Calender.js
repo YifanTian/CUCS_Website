33f8811bacdc1494e53c46791a83e3e953429e56
YifanTian <yifant@uci.edu>
2020-01-09 01:45:53 -0800

    add_something

--- client/src/App/pages/Event/Calender.js
+++ client/src/App/pages/Event/Calender.js
@@ -5,7 +5,7 @@ import M from "materialize-css";
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment'
  
-import events from './Events'
+import events from './events'
 
 import "react-big-calendar/lib/css/react-big-calendar.css"
 
